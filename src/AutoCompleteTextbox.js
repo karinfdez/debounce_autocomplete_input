0a509a190024bfb454537fa8d460ad8c148694a4
karinfdez <karinfdez@yahoo.com>
2019-08-01 22:10:11 -0400

    Change comp name to AutoCompleteTextbox

---
+++ src/AutoCompleteTextbox.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { debounce } from "throttle-debounce";
+
+class AutoCompleteTextBox extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            q: "",
+            countries: [],
+            filteredList: []
+        }
+        this.searchDebounced = debounce(600, this.autocompleteSearch);
+    }
+
+    changeQuery = ((e) => {
+        this.setState({q: e.target.value}, () => {
+            this.searchDebounced(this.state.q);
+        });
+    })
+
+    autocompleteSearch = (q) => {
+        if(q.length > 0) {
+            //This fetch is mocking a future request with different query based on user input
+            this.fetchNames()
+            .then(countriesList => {
+                let filteredList = [];
+                this.setState({countries: countriesList}, () => {
+                    const regex = new RegExp(`^${q}`,'i');
+                    filteredList = this.state.countries.filter(v => {
+                        return v.name.match(regex);
+                    })
+                })
+                this.setState({filteredList});
+            }, error => {
+                console.error(error);
+            })
+        }
+    }
+
+    fetchNames = () => {
+        return new Promise((resolve, reject) => {
+            const url = this.props.url;
+            fetch(url, {method: 'get'})
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                resolve(myJson)
+            }).catch((err) => {
+                reject(Error(err.message));
+            });
+        })
+    }
+
+    renderList() {
+        const { q } = this.state;
+        return (
+            q.length === 0 ? null : (
+                <ul className="filtered-list">
+                    {this.state.filteredList.length > 0 &&
+                    this.state.filteredList.map((countryObj) => 
+                        <li key={countryObj.alpha2Code}>{countryObj.name}</li>
+                    )}
+                </ul>
+            )
+        )
+    }
+
+    render() {
+        return (
+            <div className='wrapper'>
+                <input 
+                    placeholder="Enter a country name..." 
+                    type='text' value={this.state.q} 
+                    onChange={this.changeQuery} 
+                />
+                {this.renderList()}
+            </div>
+        )
+    }
+}
+
+export default AutoCompleteTextBox;
