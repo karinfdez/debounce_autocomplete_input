6b64b7771d2d915d72d99d6fb64154b6d742da19
karinfdez <karinfdez@yahoo.com>
2019-08-04 22:06:10 -0400

    Update with prettier file

--- src/AutoCompleteTextbox.js
+++ src/AutoCompleteTextbox.js
@@ -1,117 +1,118 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { debounce } from "throttle-debounce";
-import PropTypes from 'prop-types';
-import './AutoCompleteTextBox.css' //To ensure it loads on the head of the document
-import fetchData from './services/countries-services';
+import PropTypes from "prop-types";
+import "./AutoCompleteTextBox.css"; //To ensure it loads on the head of the document
+import fetchData from "./services/countries-services";
 
 class AutoCompleteTextBox extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
-            inputText: "",
-            countries: [],
-            filteredList: []
-        }
-        this.searchDebounced = debounce(props.debounceTime, this.autocompleteSearch);
-    }
-
-    handleChange =(e) => {
-        const text =  e.target.value;
-        this.changeQuery(text);
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: "",
+      countries: [],
+      filteredList: []
     };
-    
-    changeQuery(text) {
-        const newState = {
-            inputText: text
-        };
-        //Hides list when removing input and leaving # of elements typed
-        (text.length === this.props.amountToHideList) && 
-        (newState.filteredList = []);
-        this.setState({ ...newState }, () => {
-            if(this.state.inputText.length >= this.props.numberTypeChars) {
-                this.searchDebounced(this.state.inputText);
-            }
-        });
-    }
+    this.searchDebounced = debounce(
+      props.debounceTime,
+      this.autocompleteSearch
+    );
+  }
 
-    autocompleteSearch = (inputText) => {
-        //This fetch is mocking a request to an API that varies 
-        //everytime there is a change when user types
-        fetchData(this.props.url, this.props.fetchData)
-        .then(list => {
-            let filteredList = [];
-            const regex = new RegExp(`^${inputText}`,'i');
-            filteredList = list && list.length > 0 && list.filter(objectElem => {
-                return objectElem.name.match(regex);
-            })
-            this.setState({countries: list, filteredList});
-        })
-        .catch(error => {
-            console.error(error);
-        })
-    }
+  handleChange = e => {
+    const text = e.target.value;
+    this.changeQuery(text);
+  };
 
-    selectSuggestion(value) {
-        this.setState({
-            inputText: value, 
-            filteredList: []
-        });
-    }
+  changeQuery(text) {
+    const newState = {
+      inputText: text
+    };
+    //Hides list when removing input and leaving # of elements typed
+    text.length === this.props.amountToHideList && (newState.filteredList = []);
+    this.setState({ ...newState }, () => {
+      if (this.state.inputText.length >= this.props.numberTypeChars) {
+        this.searchDebounced(this.state.inputText);
+      }
+    });
+  }
 
-    renderFilteredList() {
-        const { inputText, filteredList } = this.state;
-        return (
-            inputText.length === 0  ? null : (
-                <ul className="list-selection">
-                    {filteredList.length > 0 && (
-                        filteredList.map((countryObj) => 
-                            <li 
-                                key={countryObj.alpha2Code} 
-                                onClick={() => 
-                                this.selectSuggestion(countryObj.name)}
-                            >
-                                {countryObj.name}
-                            </li>
-                        ))
-                    }
-                </ul>
-            )
-        )
-    }
+  autocompleteSearch = inputText => {
+    //This fetch is mocking a request to an API that varies
+    //everytime there is a change when user types
+    fetchData(this.props.url, this.props.fetchData)
+      .then(list => {
+        let filteredList = [];
+        const regex = new RegExp(`^${inputText}`, "i");
+        filteredList =
+          list &&
+          list.length > 0 &&
+          list.filter(objectElem => {
+            return objectElem.name.match(regex);
+          });
+        this.setState({ countries: list, filteredList });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-    render() {
-        return (
-            <div className='wrapper-container'>
-                <input 
-                    placeholder={this.props.placeholderMessage}
-                    value={this.state.inputText} 
-                    type='text' 
-                    onChange={this.handleChange} 
-                />
-                {this.renderFilteredList()}
-            </div>
-        )
-    }
+  selectSuggestion(value) {
+    this.setState({
+      inputText: value,
+      filteredList: []
+    });
+  }
+
+  renderFilteredList() {
+    const { inputText, filteredList } = this.state;
+    return inputText.length === 0 ? null : (
+      <ul className="list-selection">
+        {filteredList.length > 0 &&
+          filteredList.map(countryObj => (
+            <li
+              key={countryObj.alpha2Code}
+              onClick={() => this.selectSuggestion(countryObj.name)}
+            >
+              {countryObj.name}
+            </li>
+          ))}
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div className="wrapper-container">
+        <label htmlFor="search input">
+          <input
+            placeholder={this.props.placeholderMessage}
+            value={this.state.inputText}
+            type="text"
+            onChange={this.handleChange}
+          />
+        </label>
+        {this.renderFilteredList()}
+      </div>
+    );
+  }
 }
 
 AutoCompleteTextBox.propTypes = {
-    url: PropTypes.string.isRequired,
-    debounceTime: PropTypes.number,
-    numberTypeChars: PropTypes.number,
-    fetchData: PropTypes.object,
-    amountToHideList: PropTypes.number,
-    placeholderMessage: PropTypes.string
-}
+  url: PropTypes.string.isRequired,
+  debounceTime: PropTypes.number,
+  numberTypeChars: PropTypes.number,
+  fetchData: PropTypes.object,
+  amountToHideList: PropTypes.number,
+  placeholderMessage: PropTypes.string
+};
 
 AutoCompleteTextBox.defaultProps = {
-    url: '',
-    debounceTime: 250,
-    numberTypeChars: 1,
-    fetchData: {},
-    amountToHideList: 1,
-    placeholderMessage: ''
-}
-
-
+  url: "",
+  debounceTime: 250,
+  numberTypeChars: 1,
+  fetchData: {},
+  amountToHideList: 1,
+  placeholderMessage: ""
+};
 
 export default AutoCompleteTextBox;
